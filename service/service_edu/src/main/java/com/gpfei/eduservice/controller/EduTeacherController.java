07dca780cfccb968ffe913dbebc85f56cbcbfe61
JerkGPF <1040316431@qq.com>
2022-04-11 23:21:07 +0800

    集成swagger,同一返回结果

--- service/service_edu/src/main/java/com/gpfei/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/gpfei/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,12 @@
 package com.gpfei.eduservice.controller;
 
 
+import com.gpfei.commonutils.R;
 import com.gpfei.eduservice.entity.EduTeacher;
 import com.gpfei.eduservice.service.EduTeacherService;
-import lombok.val;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,7 @@ import java.util.List;
  * @author gpfei
  * @since 2022-04-11
  */
+@Api(tags="讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -25,17 +29,22 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
     //1.查询讲师所有数据
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
+    public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
     //2.逻辑删除讲师
+    @ApiOperation(value = "根据ID逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id){
         boolean flag = teacherService.removeById(id);
-        return flag;
+        if(flag)
+            return R.ok();
+        else
+            return R.error();
     }
 
 }
