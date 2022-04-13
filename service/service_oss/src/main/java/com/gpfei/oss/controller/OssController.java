6d9a25dd0dd8fec8088ae4ced07d53787373123e
JerkGPF <1040316431@qq.com>
2022-04-13 22:54:44 +0800

    集成阿里云oss，完成上传文件接口

--- service/service_oss/src/main/java/com/gpfei/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/gpfei/oss/controller/OssController.java
@@ -3,7 +3,6 @@ package com.gpfei.oss.controller;
 import com.gpfei.commonutils.R;
 import com.gpfei.oss.service.OssService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +21,6 @@ public class OssController {
     public R uploadOssFile(MultipartFile file) {
         //获取上传文件
         String url = ossService.uploadFileAvatar(file);
-
         return R.ok().data("url",url);
     }
 }
